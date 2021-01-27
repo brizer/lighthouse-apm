c63f71a1125719476ca9851d66d013090018f4a2
刘放 <362512489@qq.com>
2021-01-27 16:26:53 +0800

    feat: use puppeteer in docker

--- server/src/runner.ts
+++ server/src/runner.ts
@@ -1,27 +1,40 @@
 import lighthouse from "lighthouse";
-import fs from "fs";
-import * as chromeLauncher from "chrome-launcher";
+const {URL} = require('url');
+import puppeteer from "puppeteer";
 
 async function queryUrlTask(url: string) {
   // Catching an error
   try {
-    const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
-    const options = {
-      logLevel: "error",
-      output: "html",
-      //   onlyCategories: ["performance"],
-      port: chrome.port,
-    };
-    const runnerResult = await lighthouse(url, options);
 
+    // Use Puppeteer to launch headful Chrome and don't use its default 800x600 viewport.
+    const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: null,
+      // executablePath: '/usr/bin/chromium-browser',
+      args:['--no-sandbox']
+    });
+
+    // Wait for Lighthouse to open url, then inject our stylesheet.
+    browser.on("targetchanged", async (target) => {
+      const page = await target.page();
+      if (page && page.url() === url) {
+        await page.addStyleTag({ content: "* {color: red}" });
+      }
+    });
+
+    // Lighthouse will open the URL.
+    // Puppeteer will observe `targetchanged` and inject our stylesheet.
+    const runnerResult = await lighthouse(url, {
+      port: new URL(browser.wsEndpoint()).port,
+      output: "html",
+      logLevel: "info",
+    });
     const reportHtml = runnerResult.report;
     console.log("Report is done for", runnerResult.lhr.finalUrl);
     console.log(
       "Performance score was",
       runnerResult.lhr.categories.performance.score * 100
     );
-    fs.writeFileSync("lhreport.html", reportHtml);
-    await chrome.kill();
     return {
       reportHtml,
     };
